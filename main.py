76b024cbd18007d95e2f657ff1f16df60a7f723e
NickNameInvalid <zssmh98@outlook.com>
2022-04-06 00:28:24 -0400

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from preprocess.query_spliter import query_spliter
 import re
+import os
 from itertools import permutations, combinations
 
 qs = query_spliter('data/aol.clean', 'postings/train_queries_original.txt', 'postings/test_queries_original.txt')
 qs.query_split(10000)
-qs.save_reduced_query('postings')
+qs.enumerate_grams_disk('postings')
--- preprocess/query_spliter.py
+++ preprocess/query_spliter.py
@@ -32,21 +32,21 @@ class query_spliter:
     def save_reduced_query(self, output_dir):
         self.enumerate_grams(self.train_list, self.enum_queries_train)
         self.enumerate_grams(self.test_list, self.enum_queries_test)
-        # train_list = [open(output_dir + '/train/single_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/trian/duplets_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/train/triplets_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/train/quadruplets_reduced.txt', 'w+', encoding='utf-8')]
-        #
-        # test_list = [open(output_dir + '/test/single_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/test/duplets_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/test/triplets_reduced.txt', 'w+', encoding='utf-8'),
-        #           open(output_dir + '/test/quadruplets_reduced.txt', 'w+', encoding='utf-8')]
-        #
-        # for i in range(0, len(self.enum_queries_train)):
-        #     self.save_single_reduced_query(train_list[i], self.enum_queries_train[i])
-        #
-        # for i in range(0, len(self.enum_queries_test)):
-        #     self.save_single_reduced_query(test_list[i], self.enum_queries_test[i])
+        train_list = [open(output_dir + '/train/single_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/trian/duplets_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/train/triplets_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/train/quadruplets_reduced.txt', 'w+', encoding='utf-8')]
+
+        test_list = [open(output_dir + '/test/single_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/test/duplets_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/test/triplets_reduced.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/test/quadruplets_reduced.txt', 'w+', encoding='utf-8')]
+
+        for i in range(0, len(self.enum_queries_train)):
+            self.save_single_reduced_query(train_list[i], self.enum_queries_train[i])
+
+        for i in range(0, len(self.enum_queries_test)):
+            self.save_single_reduced_query(test_list[i], self.enum_queries_test[i])
 
     def save_single_reduced_query(self, f, input_list):
         for i in input_list:
@@ -66,4 +66,25 @@ class query_spliter:
             combs = combinations(grams, i + 1)
             for j in combs:
                 enum_list[i].append(" ".join(j))
+
+    def enumerate_grams_disk(self, output_dir):
+        train_list = [open(output_dir + '/train/single.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/train/duplets.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/train/triplets.txt', 'w+', encoding='utf-8'),
+                  open(output_dir + '/train/quadruplets.txt', 'w+', encoding='utf-8')]
+        index = 0
+        for line in self.train_list:
+            self.gram_combinations_disk(line, train_list)
+            if index % 500000 == 0:
+                print(index)
+            index += 1
+        for f in train_list:
+            f.close()
+
+    def gram_combinations_disk(self, line, f_list):
+        grams = re.split(':| ', line.strip())[1:]
+        for i in range(0, min(len(grams), 4)):
+            combs = combinations(grams, i + 1)
+            for j in combs:
+                f_list[i].write(" ".join(j) + '\n')
 
